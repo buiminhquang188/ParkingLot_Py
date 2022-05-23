6d0d6df0b2057304eaaf7484e8e2f1f1cc07c1be
thanggtrann12 <48148298+thanggtrann12@users.noreply.github.com>
2022-05-23 22:59:29 +0700

    add spot dectection

---
+++ server/post.py
@@ -0,0 +1,22 @@
+from dataclasses import replace
+import json
+from sre_constants import FAILURE, SUCCESS
+import requests
+
+from car_info.plate_detection import plate_detection, CAM_ID
+
+
+headers = {"Content-Type": "application/json; charset=utf-8"}
+url = "http://qthcmute.ddns.net:81/vehicle/parking"
+
+
+
+def post(body, box_id):
+  body = json.loads(body)
+  body['blockId'] = str(CAM_ID)
+  body['slotId'] = box_id
+  
+  if body['type'] == "PARKING":
+      resp = requests.patch(url, headers=headers, json=json.loads(str(body).replace("'", '"')))
+      print(str(body).replace("'", '"'))
+      return resp
